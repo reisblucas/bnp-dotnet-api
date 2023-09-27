1d4cce3ff8823a4ede31a35fcacfd044990ddd9c
Lucas Barbosa <88158739+reisblucas@users.noreply.github.com>
2023-09-27 16:19:53 -0300

    refactor/calc (#3)
    * feat: add multiply fn
    
    * refactor: add DefineGreater fn
    
    * feat: add defaultconditionalexpressions fn
    
    * refactor: rename variable to follow private pattern

--- src/Modules/refactor/bad_example.cs
+++ src/Modules/refactor/bad_example.cs
@@ -2,88 +2,88 @@ namespace backend_challenge.Modules.refactor;
 
 public class Calculator
 {
-    private int x;
-    private int y;
-    private int z;
-    private int result;
+    private int _x;
+    private int _y;
+    private int _z;
+    private int _result;
 
+    private bool _xIsGreaterThanY;
+    private bool _yIsGreaterThanZ;
+
+    public enum Available
+    {
+        X = 'X',
+        Y = 'Y'
+    }
+
+    private void DefaultConditionalExpressions()
+    {
+        if (_xIsGreaterThanY)
+            _x += 10;
+        else if (_yIsGreaterThanZ)
+            _y -= 5;
+        else
+            _z += 15;
+    }
+
+    private void DefineGreater(Available str, bool greater)
+    {
+        if (str == Available.X)
+            _xIsGreaterThanY = greater ? _x > _y : _x < _y;
+        if (str == Available.Y)
+            _yIsGreaterThanZ = greater ? _y > _z : _y < _z;
+    }
+
+    private void Multiply()
+    {
+        if (_xIsGreaterThanY)
+        {
+            _x *= 2;
+            _result += _x;
+        }
+        else if (_yIsGreaterThanZ)
+        {
+            _y *= 3;
+            _result += _y;
+        }
+        else
+        {
+            _z *= 4;
+            _result += _z;
+        }
+    }
+    
     public int Calc(int px, int py, int pz)
     {
-        x = px;
-        y = py;
-        z = pz;
-        result = 0;
+        _x = px;
+        _y = py;
+        _z = pz;
+        _result = 0;
 
         for (int i = 0; i < 100; i++)
         {
-            if (x > y)
-            {
-                x += 10;
-            }
-            else if (y > z)
-            {
-                y -= 5;
-            }
-            else
-            {
-                z += 15;
-            }
+            DefineGreater(Available.X, true);
+            DefaultConditionalExpressions();
 
             switch (i)
             {
                 case 0:
-                    if (x > y)
-                    {
-                        x *= 2;
-                        result += x;
-                    }
-                    else if (y > z)
-                    {
-                        y *= 3;
-                        result += y;
-                    }
-                    else
-                    {
-                        z *= 4;
-                        result += z;
-                    }
+                    Multiply();
                     break;
                 case 1:
-                    if (x < y)
-                    {
-                        x *= 2;
-                        result += x;
-                    }
-                    else if (y < z)
-                    {
-                        y *= 3;
-                        result += y;
-                    }
-                    else
-                    {
-                        z *= 4;
-                        result += z;
-                    }
+                    DefineGreater(Available.X, false);
+                    DefineGreater(Available.Y, false);
+                    Multiply();
                     break;
             }
         }
 
+        DefineGreater(Available.X, true);
         for (int j = 0; j < 50; j++)
         {
-            if (x > y)
-            {
-                x += 10;
-            }
-            else if (y > z)
-            {
-                y -= 5;
-            }
-            else
-            {
-                z += 15;
-            }
+            DefaultConditionalExpressions();
         }
 
-        return result;
+        return _result;
     }
 }
