40d076f7511c8f020e81ab1d750b0ff3c2d3ab6d
reisblucas <lucasb.reis@hotmail.com>
2023-09-27 20:28:14 -0300

    fix(repository/entity): initial value on todoStatus

--- src/Modules/todo/repository/entity.cs
+++ src/Modules/todo/repository/entity.cs
@@ -5,7 +5,7 @@ public class Todo
     public Guid id { get; set; }
     public String name { get; set; } = null!;
     public String description { get; set; } = null!;
-    public TodoStatus todoStatus { get; set; } = TodoStatus.Pending;
+    public string todoStatus { get; set; } = TodoStatus.Pending.ToString();
     public DateTime createdAt { get; set; } = DateTime.Now;
     public DateTime? completedAt { get; set; } = null;
 }
